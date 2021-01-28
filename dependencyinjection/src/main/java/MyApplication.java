c07529cbdc707b92ad96b68b2c1a0ea2f67c61c9
mariantirlea <mariantirlea@hotmail.com>
2021-01-28 21:00:15 +0200

    With dependency sent as parameter inside constructor

--- dependencyinjection/src/main/java/MyApplication.java
+++ dependencyinjection/src/main/java/MyApplication.java
@@ -1,6 +1,10 @@
 public class MyApplication {
 
-    private EmailService email = new EmailService();
+    private EmailService email;
+
+    public MyApplication(EmailService email) {
+        this.email = email;
+    }
 
     public void processMessages(String msg, String rec){
         //do some msg validation, manipulation logic etc
--- dependencyinjection/src/main/java/MyLegacyTest.java
+++ dependencyinjection/src/main/java/MyLegacyTest.java
@@ -2,7 +2,7 @@ public class MyLegacyTest {
 
     public static void main(String[] args){
 
-        MyApplication app = new MyApplication();
+        MyApplication app = new MyApplication(new EmailService());
         app.processMessages("Hi John", "john@abc.com");
 
     }
